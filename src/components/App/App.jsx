5eb4e6a2a0ae337e85998eb5ec5a2a7786717366
MathildeB <thithildeb@gmail.com>
2023-01-20 13:27:24 +0100

    config

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import './styles.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Bonjour</h1>
+    </div>
+  );
+}
+
+
+App.propTypes = {
+  
+};
+
+export default React.memo(App);
