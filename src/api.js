a023767420075a3b8c071a666391a62eb2a9edc2
Bernard Arroues <arroues.bernard@gmail.com>
2023-01-24 16:00:31 +0100

    Update to env file

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // création d'une instance d'axios
 const api = axios.create({
-    baseURL:`${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/v1`
+    baseURL:`${process.env.REACT_APP_API_URL}/v1`
 });
 
 
