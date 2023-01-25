35fc36ac6c9b5813d389ed762fe4820bcf965907
tfptmaster <tfptmaster@gmail.com>
2023-01-25 18:09:50 +0100

    fix phone and password bug

--- src/components/DashboardVolunteerCreation/DashboardVolunteerCreation.jsx
+++ src/components/DashboardVolunteerCreation/DashboardVolunteerCreation.jsx
@@ -9,7 +9,7 @@ import { useSelector } from "react-redux";
 import "./styles.scss";
 
 function DashboardVolunteerCreation() {
-  const { register, errors, handleSubmit } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = (data) => {
     console.log("RESULT", data);
     alert(JSON.stringify(data));
@@ -25,39 +25,39 @@ function DashboardVolunteerCreation() {
       <h1 className="title-page">Bonjour {firstName} </h1>
       <div className="dashboard-container">
         <Form className="dashboard-container-form" onSubmit={handleSubmit(onSubmit)}>
-          <Form.Group className="mb-3" controlId="firstname">
+          <Form.Group className="mb-3" controlId="volunteerFirstname">
             <Form.Label>Prénom</Form.Label>
             <Form.Control
               type="text"
               {...register("First name", { required: true })}
             />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="lastName">
+          <Form.Group className="mb-3" controlId="volunteerLastName">
             <Form.Label>Nom</Form.Label>
             <Form.Control
               type="text"
               {...register("Last name", { required: true })}
             />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="email">
+          <Form.Group className="mb-3" controlId="volunteerEmail">
             <Form.Label>Email</Form.Label>
             <Form.Control
               type="email"
               {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
             />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="tel">
+          <Form.Group className="mb-3" controlId="VolunteerTel">
             <Form.Label>Numéro de telephone</Form.Label>
             <Form.Control
               type="tel"
-              {...register("Mobile number", {required: true, minLength: 12, maxLength: 12})}
+              {...register("Mobile number", {required: true, minLength: 8, maxLength: 12})}
             />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formGroupPassword">
+          <Form.Group className="mb-3" controlId="volunteerPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" />
+            <Form.Control type="password" {...register("Password", {required: true, minLength: 8})} />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formGroupPassword">
+          <Form.Group className="mb-3" controlId="volunteerLevel">
             <Form.Label>Niveau d'expérience du bénévole</Form.Label>
             <Form.Select aria-label="Default select example" name="level" {...register("level", { required: true })}>
                 <option value="beginner">Débutant</option>
@@ -67,52 +67,7 @@ function DashboardVolunteerCreation() {
           </Form.Group>
           <Button variant="primary" type="submit">Créer Bénévole</Button>
         </Form>
-{/* 
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <label>Prénom</label>
-          <input type="text" {...register("First name", { required: true })} />
 
-          <label>Nom</label>
-          <input
-            type="text"
-            {...register("Last name", { required: true, maxLength: 100 })}
-          />
-          <label>Email</label>
-          <input
-            type="text"
-            {...register("Email", {
-              required: true,
-              pattern:
-                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            })}
-          />
-          <label>Numéro de telephone</label>
-          <input
-            type="tel"
-            {...register("phone", {
-              required: true,
-              maxLength: 11,
-              minLength: 8,
-            })}
-          />
-          <label>Niveaux d'expérience</label>
-          <select
-            className="form-select"
-            name="level"
-            {...register("level", { required: true })}
-          >
-            <option value="beginner">Débutant</option>
-            <option value="medium">Moyen</option>
-            <option value="expert">Expérimenté</option>
-          </select>
-          <label>Mot de passe</label>
-          <input
-            type="password"
-            {...register("Password", { required: true, maxLength: 8 })}
-          />
-
-          <input type="submit" />
-        </form> */}
       </div>
     </>
   );
