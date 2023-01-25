a412a0a5e412a8c68d5579ba2edd4547a636c7f1
tfptmaster <tfptmaster@gmail.com>
2023-01-25 10:10:55 +0100

    dashboard structure finished

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -18,33 +18,35 @@ import './Dashboard.scss';
 
 function Dashboard() {
 
+  // récupération prénom de l'admin
   const firstName = useSelector((fullstate) => fullstate.loginSettings.firstName);
 
   return (
     <>
       <h1 className='title-page'>Bonjour {firstName} </h1>
       <div className='dashboard-container'>
-      <CardGroup>
-      <Card>
-        <Card.Img className='dashboard-container' variant="top" src={DogAndCat} />
-        <Card.Body>
-          <Button href="/create/card" variant="primary" type="submit">Liste des Animaux</Button>
-        </Card.Body>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src={womenWalkingDog} />
-        <Card.Body>
-          <Button href="/create/card" variant="primary" type="submit">Créer un Bénévole</Button>
-        </Card.Body>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src={DogHugsCat} />
-        <Card.Body>
-          <Button href="/create/card" variant="primary" type="submit">Créer Animal</Button>
-        </Card.Body>
-      </Card>
-    </CardGroup>
+        <CardGroup>
+          <Card>
+            <Card.Img className='dashboard-container' variant="top" src={DogAndCat} />
+            <Card.Body>
+              <Button href="/create/card" variant="primary" type="submit">Liste des Animaux</Button>
+            </Card.Body>
+          </Card>
 
+          <Card>
+            <Card.Img variant="top" src={womenWalkingDog} />
+            <Card.Body>
+              <Button href="/create/card" variant="primary" type="submit">Créer un Bénévole</Button>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Img variant="top" src={DogHugsCat} />
+            <Card.Body>
+              <Button href="/create/card" variant="primary" type="submit">Créer Animal</Button>
+            </Card.Body>
+          </Card>
+        </CardGroup>
       </div>
     </>
   );
