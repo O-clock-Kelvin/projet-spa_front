cadf90b3d94a4265a3c376415e444e6e8b9fb5db
Bernard Arroues <arroues.bernard@gmail.com>
2023-01-27 19:53:32 +0100

    Fix footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,6 +7,7 @@ import Icon from '../../assets/icon.svg';
 
 //SCSS
 import "./Footer.scss";
+import { Link } from 'react-router-dom';
 
 function Footer() {
 
@@ -14,10 +15,10 @@ return (
     <Container className='footer' fluid>
       <Row className='footer-row'>
         <Col sm={12} md={3} lg={2} className="align-self-center">
-          <a href="">Mentions Légales</a>
+          <Link to="">Mentions Légales</Link>
         </Col>
         <Col sm={12} md={3} lg={2} className="align-self-center">
-          <a href="">Politique de confidentialité</a>
+          <Link to="">Politique de confidentialité</Link>
         </Col>   
         <Col sm={12} md={2} lg={4} className="align-self-center">
           <div className="text-center">
