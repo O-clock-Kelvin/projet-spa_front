cee2d5e189260a75b05a246242642389857bdd2d
Bernard Arroues <arroues.bernard@gmail.com>
2023-01-30 20:03:42 +0100

    Hotfix: button to start walk not rendering when animal had no previous walks

--- src/components/WalkStartButton/index.js
+++ src/components/WalkStartButton/index.js
@@ -48,122 +48,115 @@ const StartWalkButton = ({ animal }) => {
 	});
 
 	if (animal && admin == false) {
-		if (lastWalk) {
-			/**
-			 * durée de la balade max 1h donc on check si la derniere sortie remonte à plus d'une heure, pour déterminer si l'animal est en cours de balade
-			 * On vérifie également que l'animal n'est pas déjà sorti dans la journée
-			 */
+		/**
+		 * durée de la balade max 1h donc on check si la derniere sortie remonte à plus d'une heure, pour déterminer si l'animal est en cours de balade
+		 * On vérifie également que l'animal n'est pas déjà sorti dans la journée
+		 */
 
+		if (
+			lastWalk == undefined ||
+			(DateTime.fromISO(lastWalk?.date).plus({ hour: 1 }) <= DateTime.now() &&
+				DateTime.fromISO(lastWalk?.date) <= DateTime.now().startOf('day') &&
+				startedWalk == undefined)
+		) {
 			if (
-				DateTime.fromISO(lastWalk.date).plus({ hour: 1 }) <= DateTime.now() &&
-				DateTime.fromISO(lastWalk.date) <= DateTime.now().startOf('day') &&
-				startedWalk == undefined
+				experienceConverter(experience) >=
+				experienceConverter(animal.volunteer_experience)
 			) {
-				if (
-					experienceConverter(experience) >=
-					experienceConverter(animal.volunteer_experience)
-				) {
-					return (
-						<>
-							<div
-								onClick={() => setShowModal(true)}
-								role='button'
-								tabIndex='0'
-								style={buttonStyle}
-							>
-								Partir en balade
-							</div>
-
-							{/* Modale de confirmation de la balade */}
-							<Modal show={showModal} onHide={() => setShowModal(false)}>
-								<Modal.Header closeButton>
-									<Modal.Title>confirmation de balade</Modal.Title>
-								</Modal.Header>
-								<Modal.Body>
-									Etes vous sur de vouloir partir en balade ?
-								</Modal.Body>
-								<Modal.Footer>
-									<Button
-										variant='secondary'
-										onClick={() => setShowModal(false)}
-									>
-										Annuler
-									</Button>
-									{isLoading ? (
-										'Loading...'
-									) : (
-										<Button
-											variant='primary'
-											onClick={() => {
-												startWalk({ animal_id: animal.id });
-											}}
-										>
-											Oui
-										</Button>
-									)}
-									{error && (
-										<div style={buttonStyle}>
-											Une erreur est survenue, merci de retenter plus tard
-										</div>
-									)}
-								</Modal.Footer>
-							</Modal>
-						</>
-					);
-				} else {
-					return (
-						<div style={buttonStyle}>
-							Vous ne pouvez pas partir en balade (expérience insuffisante)
+				return (
+					<>
+						<div
+							onClick={() => setShowModal(true)}
+							role='button'
+							tabIndex='0'
+							style={buttonStyle}
+						>
+							Partir en balade
 						</div>
-					);
-				}
+
+						{/* Modale de confirmation de la balade */}
+						<Modal show={showModal} onHide={() => setShowModal(false)}>
+							<Modal.Header closeButton>
+								<Modal.Title>confirmation de balade</Modal.Title>
+							</Modal.Header>
+							<Modal.Body>
+								Etes vous sur de vouloir partir en balade ?
+							</Modal.Body>
+							<Modal.Footer>
+								<Button variant='secondary' onClick={() => setShowModal(false)}>
+									Annuler
+								</Button>
+								{isLoading ? (
+									'Loading...'
+								) : (
+									<Button
+										variant='primary'
+										onClick={() => {
+											startWalk({ animal_id: animal.id });
+										}}
+									>
+										Oui
+									</Button>
+								)}
+								{error && (
+									<div style={buttonStyle}>
+										Une erreur est survenue, merci de retenter plus tard
+									</div>
+								)}
+							</Modal.Footer>
+						</Modal>
+					</>
+				);
+			} else {
+				return (
+					<div style={buttonStyle}>
+						Vous ne pouvez pas partir en balade (expérience insuffisante)
+					</div>
+				);
+			}
+		} else {
+			if (
+				startedWalk ||
+				(lastWalk.user_id == id &&
+					lastWalk.end_date == undefined &&
+					DateTime.fromISO(lastWalk.date).plus({ hour: 1 }) >= DateTime.now())
+			) {
+				return (
+					<>
+						<WalkEditor
+							walk={startedWalk || lastWalk}
+							show={showEndEditor}
+							endingWalk={true}
+							onClose={() => setShowEndEditor(false)}
+							onUpdate={() => {
+								setStartedWalk(undefined);
+								setLastWalk(undefined);
+							}}
+						/>
+						<div
+							style={buttonStyle}
+							onClick={() => {
+								setShowEndEditor(true);
+							}}
+							role='button'
+							tabIndex='0'
+						>
+							Terminer la balade
+						</div>
+					</>
+				);
 			} else {
 				if (
-					startedWalk ||
-					(lastWalk.user_id == id &&
-						lastWalk.end_date == undefined &&
-						DateTime.fromISO(lastWalk.date).plus({ hour: 1 }) >= DateTime.now())
+					DateTime.fromISO(lastWalk.date).plus({ hour: 1 }) >= DateTime.now() &&
+					lastWalk.end_date == undefined
 				) {
 					return (
-						<>
-							<WalkEditor
-								walk={startedWalk || lastWalk}
-								show={showEndEditor}
-								endingWalk={true}
-								onClose={() => setShowEndEditor(false)}
-								onUpdate={() => {
-									setStartedWalk(undefined);
-									setLastWalk(undefined);
-								}}
-							/>
-							<div
-								style={buttonStyle}
-								onClick={() => {
-									setShowEndEditor(true);
-								}}
-								role='button'
-								tabIndex='0'
-							>
-								Terminer la balade
-							</div>
-						</>
+						<div style={buttonStyle}>Cet animal est en cours de sortie</div>
 					);
 				} else {
-					if (
-						DateTime.fromISO(lastWalk.date).plus({ hour: 1 }) >=
-							DateTime.now() &&
-						lastWalk.end_date == undefined
-					) {
-						return (
-							<div style={buttonStyle}>Cet animal est en cours de sortie</div>
-						);
-					} else {
-						return (
-							<div style={buttonStyle}>
-								Cet animal est déjà sorti aujourd'hui
-							</div>
-						);
-					}
+					return (
+						<div style={buttonStyle}>Cet animal est déjà sorti aujourd'hui</div>
+					);
 				}
 			}
 		}
