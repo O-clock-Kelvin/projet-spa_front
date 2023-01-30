0acbc15ed0db655b4969e09febd37695c8713f18
angelique pinchon <ange.pinchon@gmail.com>
2023-01-30 11:17:02 +0100

    add-link-main-menu

---
+++ src/components/ListAnimals/ListAnimals.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ListAnimals() {
+    return (
+      <>
+          <h1>Tout o poils</h1>
+          <div>ListAnimals</div>
+      </>
+    );
+  }
+  
+  export default React.memo(ListAnimals);
+  
