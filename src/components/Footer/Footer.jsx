a35b3c7560f58eb428dee73708fa595834e6d245
ElBernie <arroues.bernard@gmail.com>
2023-02-07 21:01:29 +0100

    Fix: Footer location

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,37 +4,37 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Icon from '../../assets/icon.svg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import './Footer.scss';
 
 function Footer() {
+	const location = useLocation();
+
 	return (
 		<>
-		{location.pathname != "/login" ? (
-		<Container className='footer' fluid>
-		
-			<div className='container'>
-				<Row className='footer-row'>
-					<Col sm={12} md={3} lg={2} className='align-self-center'>
-						<Link to='/mentions-legales'>Mentions Légales</Link>
-					</Col>
-					<Col sm={12} md={3} lg={2} className='align-self-center'>
-						<Link to=''>Politique de confidentialité</Link>
-					</Col>
-					<Col sm={12} md={2} lg={4} className='align-self-center'>
-						<div className='text-center'>
-							<img className='footer-icon' src={Icon} alt='Tout O Poils' />
-						</div>
-					</Col>
-					<Col sm={12} md={3} lg={4} className='align-self-center'>
-						<p>Copyright - 2023</p>
-					</Col>
-				</Row>
-			</div>
-			
-		</Container>
-		) : null}
+			{location.pathname != '/login' ? (
+				<Container className='footer' fluid>
+					<div className='container'>
+						<Row className='footer-row'>
+							<Col sm={12} md={3} lg={2} className='align-self-center'>
+								<Link to='/mentions-legales'>Mentions Légales</Link>
+							</Col>
+							<Col sm={12} md={3} lg={2} className='align-self-center'>
+								<Link to=''>Politique de confidentialité</Link>
+							</Col>
+							<Col sm={12} md={2} lg={4} className='align-self-center'>
+								<div className='text-center'>
+									<img className='footer-icon' src={Icon} alt='Tout O Poils' />
+								</div>
+							</Col>
+							<Col sm={12} md={3} lg={4} className='align-self-center'>
+								<p>Copyright - 2023</p>
+							</Col>
+						</Row>
+					</div>
+				</Container>
+			) : null}
 		</>
 	);
 }
